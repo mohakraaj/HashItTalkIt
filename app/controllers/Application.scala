d0295c561d10eb4a6bd2b7751532e5b7de5c5288
mohakraaj <mohakraaj@gmail.com>
2015-06-12 04:12:34 -0700

    Adding logging, web socket bugs
    Multiple bugs in websocket
    - Map key not found error
    - Json Parsing issue
    
    Created new method to message object creation
    
    Restricting user to access chat page directly
    
    Enhanced logging : more logging, redirecting to file appender, creating
    logger for each class.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ import models._
 import models.UserActor._
 import akka.actor.ActorRef
 import models.RoomData
-import play.Logger
+import play.api.Logger
 import play.api.data.Forms._
 import play.api.data._
 import play.api.libs.json.JsValue
@@ -25,6 +25,8 @@ class Application extends Controller {
 
   val roomName = "ROOMNAME"
 
+  val logger = Logger(this.getClass())
+
   // Form structure to collect Room details
   val roomDetailsForm = Form(
     mapping(
@@ -33,44 +35,65 @@ class Application extends Controller {
   )
 
   //Load home page with Room details form
-  def home() = Action {
-    Ok(views.html.home(roomDetailsForm))
-  }
-
-  // Sets up page for chatting with the webSocket
-  def chat = Action { implicit request =>
-
+  // TODO : Redirect to chat room if the user has already added Room
+  def home() = Action { implicit request =>
     val uid = request.session.get(UID).getOrElse {
       counter = counter + 1
       counter.toString()
     }
+    logger.debug("User uid: " + uid + " has started the application")
+    Ok(views.html.home(roomDetailsForm)).withSession(request.session + (UID -> uid))
+  }
 
-    Ok(views.html.chat("Your new application is ready.")).withSession(request.session +(UID, uid))
+  // Sets up page for chatting with the webSocket
+  def chat = Action { implicit request =>
+    request.session.get(roomName) match {
+      case (Some(room: String)) =>  {
+        logger.debug("Sending user uid : "+ request.session.get(UID) +" to chat room")
+        Ok(views.html.chat("Your new application is ready."))
+      }
+      case None => {
+        logger.debug("Redirecting user to home page")
+        Redirect(routes.Application.home())
+      }
+    }
   }
 
   // Bind user form to room details and store it in hashmap
+  //TODO : Make room details as FlashScope
   def addRoomDetails() = Action { implicit request =>
     roomDetailsForm.bindFromRequest.fold(
       formWithErrors => {
+        logger.error("User has entered in compatible form values")
         BadRequest(views.html.home(formWithErrors))
       },
       RoomData => {
-        Redirect(routes.Application.chat()).withSession(request.session +(roomName, RoomData.name))
+        logger.debug("Redirecting user to chat room")
+        Redirect(routes.Application.chat()).withSession(request.session + (roomName -> RoomData.name))
       }
     )
   }
 
+  // Method to attach websockets with Actor class
   def ws = WebSocket.tryAcceptWithActor[JsValue, JsValue] { implicit request =>
+    logger.debug("Initiating websockets with session value :" + request.session.get(UID))
+    logger.debug("Initiating websockets with roomName : " + request.session.get(roomName))
+
     Future.successful((request.session.get(UID), request.session.get(roomName)) match {
       case (Some(uid: String), Some(room: String)) => Right {
         UserActor.props(uid, room)
       }
       case (None, x) => {
-        Logger.warn("Session has no Room name, but has userId: " + x)
+        logger.error("Session has no Room name, but has userId: " + x)
         Left(Forbidden)
       }
       case (x, None) => {
-        Logger.warn("Session has no UserID, but has roomName: " + x)
+        logger.error("Session has no UserID, but has roomName: " + x)
+        Left(Forbidden)
+      }
+      case (None, None) => {
+        logger.error("No session values are available")
+        //TODO : Warn user about cookies
         Left(Forbidden)
       }
     })
--- app/models/UserActor.scala
+++ app/models/UserActor.scala
@@ -4,8 +4,9 @@ import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.event.LoggingReceive
 import play.api.Logger
 import play.api.libs.json.JsValue
+import play.api.libs.json.Json
 
-import scala.collection.mutable._
+import scala.collection.mutable.HashMap
 import scala.xml.Utility
 
 /** *****************************************************
@@ -18,33 +19,43 @@ import scala.xml.Utility
   * ******************************************************/
 class UserActor(uid : String, roomName: String, out: ActorRef) extends Actor with ActorLogging {
 
+  val logger = Logger(this.getClass())
 
   override def preStart =  {
+    logger.info("Creating new instance of UserActor for roomName: "+ roomName + " and UID: " + uid)
     UserActor.updateRoomUserMapping(this.self , roomName)
   }
 
   def receive() = LoggingReceive {
     case Message(uid,msg) => {
-      //Logger.info("User: " + uid+ " has received the message")
-      out ! msg
+      logger.debug("User: " + uid+ " has received the message")
+      out ! Json.obj("type" -> "message", "uid" -> uid, "msg" -> msg)
     }
     case js: JsValue =>  {
-      //Logger.info("User: " + uid+ " has sent the message")
-      val msg = (js \ "msg").validate[String] map {
-        Utility.escape(_)
-      } map {
-        Message(uid, _)
-      }
+      logger.debug("User uid: "+ uid +" has sent the message" )
+      logger.debug("Retrieving all the users mapped to the room name: " + roomName)
 
       UserActor.userRoomMapping.get(roomName) match {
-        case users : List[ActorRef] => users.toList.map(x => x ! msg)
-        case None => out ! "No one else in Room"
+        case Some(users: List[ActorRef]) => {
+          if (users.length ==1) {
+            logger.info("Only user available on chat room : " + roomName)
+            //TODO : NOTIFY user about this
+          }
+          users.toList.map(x => x ! createMessageObject(js))
+        }
+        case None => {
+          logger.error("mapping for the roomname is resulting in no users")
+        }
       }
 
     }
     case others => {
-      Logger.warn("User Actor instance for the user: "+ uid + " has received an unexpected message")
+      logger.warn("User Actor instance for the user: "+ uid + " has received an unexpected message")
     }
+  }
+
+  private def createMessageObject(jsValue: JsValue): Message = {
+    return Message(uid, Utility.escape((jsValue\ "msg").as[String]))
   }
 }
 
@@ -53,28 +64,28 @@ case class RoomData(name: String)
 case class Message(uid: String, msg: String)
 
 object UserActor {
+
+  val logger = Logger(this.getClass())
+
   // HashMap of (RoomName) -> List[UserActors]
-  val userRoomMapping = HashMap[String, List[ActorRef]]()
+  val userRoomMapping = HashMap[String, List[ActorRef]]().withDefaultValue(List())
 
   // method to create an Actor for user Instance
   def props(uid: String, roomName: String)(out: ActorRef) = Props(new UserActor(uid,roomName,out))
 
   //Method to update HashMap for roomName -> userActor
-  def updateRoomUserMapping(user: ActorRef, roomName: String): Boolean = {
+  def updateRoomUserMapping(user: ActorRef, roomName: String) = {
+
+    logger.info("Updating user mapping for room name: " + roomName)
+
     //Restrain user from adding himself to multiple rooms
     if (userRoomMapping.exists(_._2.exists(_ == user))) {
-      Logger.warn("User can participate in only one secret conversation")
-      false
+      logger.error("User can participate in only one secret conversation")
     }
     else {
       // Create a new Map if roomName is not already mapped, orelse add to the existing list
-      if (userRoomMapping.exists(_._1 != roomName)) {
-        userRoomMapping(roomName) = List(user)
-      }
-      else {
+        logger.info("Updating useractor to the existing mapping, for room name :" + roomName)
         userRoomMapping(roomName) = user :: userRoomMapping(roomName)
-      }
-      true
     }
   }
 }
